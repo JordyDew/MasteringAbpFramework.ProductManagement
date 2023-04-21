d270a32031660d2929ecc40a97c929ec87db6354
Jordy Deweer <jordy@dewsystems.be>
2023-04-21 22:42:16 +0200

    Implement test for getlist method of product app service

--- test/ProductManagement.Application.Tests/Products/ProductAppService_Tests.cs
+++ test/ProductManagement.Application.Tests/Products/ProductAppService_Tests.cs
@@ -1,12 +1,31 @@
-﻿namespace ProductManagement.Products;
+﻿using Shouldly;
+
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace ProductManagement.Products;
 
 public class ProductAppService_Tests
-    :ProductManagementApplicationTestBase
+    : ProductManagementApplicationTestBase
 {
     private readonly IProductAppService _productAppService;
 
     public ProductAppService_Tests()
     {
-            _productAppService=GetRequiredService<IProductAppService>();
+        _productAppService = GetRequiredService<IProductAppService>();
+    }
+
+    [Fact]
+    public async Task Should_Get_List_Of_Products()
+    {
+        //Act
+        var output = await _productAppService.GetListAsync(new Volo.Abp.Application.Dtos.PagedAndSortedResultRequestDto());
+        //arrange
+        output.TotalCount.ShouldBe(3);
+        output.Items.ShouldContain(
+    x => x.Name.Contains("Acme Monochrome Laser                     Printer")
+);
+
     }
 }
