c855ed86e261f8ce9491d2937f69c3d0ac1f65f6
Jordy Deweer <jordy@dewsystems.be>
2023-04-21 22:01:40 +0200

    Implement the getlist method of the app service interface

---
+++ src/ProductManagement.Application/Products/ProductAppService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Repositories;
+
+namespace ProductManagement.Products;
+
+public class ProductAppService
+    : ProductManagementAppService, IProductAppService
+{
+    private readonly IRepository<Product, Guid> _productRepository;
+
+    public ProductAppService(IRepository<Product, Guid> productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<PagedResultDto<ProductDto>> GetListAsync(PagedAndSortedResultRequestDto input)
+    {
+        var queryable = await _productRepository.WithDetailsAsync(x => x.Category);
+        queryable = queryable
+            .Skip(input.SkipCount)
+            .Take(input.MaxResultCount)
+            .OrderBy(input.Sorting ?? nameof(Product.Name));
+
+        var products = await AsyncExecuter.ToListAsync(queryable);
+        var totalCount  =await AsyncExecuter.CountAsync(_productRepository);
+        return new PagedResultDto<ProductDto>(
+                totalCount,
+                ObjectMapper.Map<List<Product>, List<ProductDto>>(products));
+    }
+}
