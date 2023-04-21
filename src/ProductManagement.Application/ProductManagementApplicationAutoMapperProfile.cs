9e8f1482584e50b47ec0f1b602e4b5d333dc7578
Jordy Deweer <jordy@dewsystems.be>
2023-04-21 22:25:04 +0200

    Add automapper profile for products

--- src/ProductManagement.Application/ProductManagementApplicationAutoMapperProfile.cs
+++ src/ProductManagement.Application/ProductManagementApplicationAutoMapperProfile.cs
@@ -9,5 +9,7 @@ public class ProductManagementApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+
+        CreateMap<Product, ProductDto>();
     }
 }
