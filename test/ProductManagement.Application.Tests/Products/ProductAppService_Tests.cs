c1d8908745bfe0c5d32bd56df904b38f07f0f2b8
Jordy Deweer <jordy@dewsystems.be>
2023-04-21 22:35:06 +0200

    Add test class for the products app service

---
+++ test/ProductManagement.Application.Tests/Products/ProductAppService_Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductManagement.Products;
+
+public class ProductAppService_Tests
+    :ProductManagementApplicationTestBase
+{
+    private readonly IProductAppService _productAppService;
+
+    public ProductAppService_Tests()
+    {
+            _productAppService=GetRequiredService<IProductAppService>();
+    }
+}
