a0cfe93452a40d3d272bc8f6d013a36a93271616
Jordy Deweer <jordy@dewsystems.be>
2023-04-21 21:03:56 +0200

    Add data seeding contributor

---
+++ src/ProductManagement.Domain/Data/ProductManagementDataSeedContributor.cs
@@ -0,0 +1,31 @@
+﻿using ProductManagement.Categories;
+
+using System;
+using System.Threading.Tasks;
+
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace ProductManagement.Data;
+
+public class ProductManagementDataSeedContributor
+    : IDataSeedContributor, ITransientDependency
+{
+    private readonly IRepository<Category, Guid> _categoryRepository;
+    private readonly IRepository<Product, Guid> _productRepository;
+
+    public ProductManagementDataSeedContributor(
+        IRepository<Category, Guid> categoryRepository,
+        IRepository<ProductManagement, Guid> productRepository)
+    {
+        _productRepository = _productRepository;
+        _categoryRepository = _categoryRepository;
+    }
+
+    public Task SeedAsync(DataSeedContext context)
+    {
+
+        return Task.CompletedTask;
+    }
+}
