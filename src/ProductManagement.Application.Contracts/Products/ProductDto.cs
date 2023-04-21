7491594582d8d40df766248b8c79d1c8e95b2d79
Jordy Deweer <jordy@dewsystems.be>
2023-04-21 21:37:55 +0200

    Add product dto

---
+++ src/ProductManagement.Application.Contracts/Products/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Volo.Abp.Application.Dtos;
+
+namespace ProductManagement.Products;
+
+public class ProductDto : AuditedEntityDto<Guid>
+{
+    public Guid CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public bool IsFreeCargo { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public ProductStockState StockState { get; set; }
+}
